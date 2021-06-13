d2d7ae74b8ca08f3fe18072d8799f0edeb7e7b36
saperez17 <saperez@unicauca.edu.co>
2021-06-13 17:21:13 -0500

    added notification component to successful operations

--- src/App.js
+++ src/App.js
@@ -65,10 +65,21 @@ const Persons = ({ persons, filter, setPersons }) => {
     </div>
   );
 };
+
+const Notification = ({message})=>{
+  if (message===null) return null
+  return(
+    <div className="notification">
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newContact, setNewContact] = useState({ name: "", number: "" });
   const [filter, setFilter] = useState("");
+  const [notificationMessage, setNotificationMessage] = useState(null)
 
   useEffect(()=>{
     personService.getAll()
@@ -92,6 +103,10 @@ const App = () => {
     if (!userSearch) {
       personService.create(newContact)
       .then(createdPerson =>{
+        setNotificationMessage(`${newContact.name} added successfully!`)
+        setTimeout(()=>{
+          setNotificationMessage(null)
+        }, 5000)
         setPersons((prevState) => [...prevState, newContact])
         setNewContact({ name: "", number: "" });
       })
@@ -100,8 +115,11 @@ const App = () => {
       if (userConfirmation){
         userSearch.number = newContact.number
         personService.update(userSearch.id, userSearch)
-        .then(contactUpdateResponse =>{
-          console.log(contactUpdateResponse);
+        .then(contactUpdateResponse =>{          
+          setNotificationMessage(`${userSearch.name} updated successfully!`)
+          setTimeout(()=>{
+            setNotificationMessage(null)
+          }, 5000)
           setNewContact({ name: "", number: "" });
         })
       }
@@ -116,7 +134,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <Notification  message={notificationMessage}/>
       <Filter onChangeSearch={onChangeSearch} filterInputValue={filter} />
 
       <h2>add a new</h2>
